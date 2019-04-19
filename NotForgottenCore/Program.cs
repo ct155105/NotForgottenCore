ec213ac6edf9f0380b5f14b1d11ba7147c2ac8a6
Chris Teuschler <ChrisTeuschler@Gmail.com>
2019-04-18 22:48:12 -0400

    Final 2019 COmmit. Horse Create View / Table View disabled

--- NotForgottenCore/Program.cs
+++ NotForgottenCore/Program.cs
@@ -19,6 +19,7 @@ namespace NotForgottenCore
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
